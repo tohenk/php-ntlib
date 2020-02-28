8fd21953c4a532a2a5f019743194eaa59b2f7565
Toha <tohenk@yahoo.com>
2020-02-28 13:05:59 +0700

    Beautifier return original ignore case.
    Signed-off-by: Toha <tohenk@yahoo.com>

--- tests/BeautifierTest.php
+++ tests/BeautifierTest.php
@@ -11,7 +11,7 @@ class BeutifierTest extends BaseTest
         $this->assertEquals('Beautify Me', Beautifier::beautify('BEAUTIFY me'), 'BEAUTIFY me => Beautify Me');
         $this->assertEquals('Year MMIV', Beautifier::beautify('YEAR MMIV', Beautifier::IGNORE_ROMAN), 'Beautify should preserve roman');
         Beautifier::addIgnore(array('ME', 'YOU'));
-        $this->assertEquals('Beautify me And YOU', Beautifier::beautify('BEAUTIFY me and YOU', Beautifier::IGNORE_INLIST), 'Beautify should ignore if text is in exception');
-        $this->assertEquals('Beautify Me And You Xx', Beautifier::beautify('BEAUTIFY Me and You Xx', Beautifier::IGNORE_INLIST | Beautifier::IGNORE_ROMAN), 'Beautify should ignore if flag specified');
+        $this->assertEquals('Beautify ME And YOU', Beautifier::beautify('BEAUTIFY me and YOU', Beautifier::IGNORE_INLIST), 'Beautify should ignore if text is in exception');
+        $this->assertEquals('Beautify ME And YOU Xx', Beautifier::beautify('BEAUTIFY Me and You Xx', Beautifier::IGNORE_INLIST | Beautifier::IGNORE_ROMAN), 'Beautify should ignore if flag specified');
     }
 }
